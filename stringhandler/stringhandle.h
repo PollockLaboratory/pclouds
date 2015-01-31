811b25eeb366abfbb862200570b8df947eec0a21
Daniel L. Polanco <dlpolanco05@gmail.com>
2015-01-31 16:16:37 -0700

    Remove unnecessary / unused functions from stringhandle

--- stringhandler/stringhandle.h
+++ stringhandler/stringhandle.h
@@ -5,34 +5,8 @@
 
 namespace pclouds {
 
-void strright(int i, char *source, char *target);
-
-int straddright(char *source, char *target, char *add);
-
-void strleft(int i, char* source, char* target);
-
-int strcompare(int i, char *source, char *target);
-
-int strreplaceright(char *source, char *target, char *replace);
-
-void getsubstring(char* pchSource, char *pchTarget, int nStart, int nEnd);
-
-int stringcompare(char *pchSource, char *pchTarget);
-
-int stringcompare(const char *pchSource, const char *pchTarget);
-
-int stringinitial(char *pchSource, int iLength);
-
-int stringcopy(char *pchSource, char *pchTarget, int iLength);
-
 void getreversecomplement(const char* pchSource, char* pchTarget);
 std::string getreversecomplement(std::string kmer);
-
-int stringtonumber(const std::string& m_strTemp);
-
-unsigned int stringtolargenumber(const std::string& m_strTemp);
-
-void stringtoarray(const std::string& src, char* target);
 
 bool is_SSR(const char *kmer);
 
