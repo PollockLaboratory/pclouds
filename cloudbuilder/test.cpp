26e569864c081c8d7c639d57f4b29a377a1ce4d9
Daniel L. Polanco <dlpolanco05@gmail.com>
2015-02-03 13:04:42 -0700

    Remove fake factorial test scenario

--- cloudbuilder/test.cpp
+++ cloudbuilder/test.cpp
@@ -2,9 +2,9 @@
 #include "../catch/single_include/catch.hpp"
 #include "./build_clouds.hpp"
 
-TEST_CASE( "Factorials are computed", "[factorial]" ) {
-	REQUIRE( pclouds::Factorial(0) == 1 );
-	REQUIRE( pclouds::Factorial(1) == 1 );
-	REQUIRE( pclouds::Factorial(2) == 2 );
-	REQUIRE( pclouds::Factorial(3) == 6 );
-}
+//TEST_CASE( "Factorials are computed", "[factorial]" ) {
+//	REQUIRE( pclouds::Factorial(0) == 1 );
+//	REQUIRE( pclouds::Factorial(1) == 1 );
+//	REQUIRE( pclouds::Factorial(2) == 2 );
+//	REQUIRE( pclouds::Factorial(3) == 6 );
+//}
