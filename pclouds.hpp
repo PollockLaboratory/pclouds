ef58e0e6e85beec69c405209e3b11f25e3c1428c
Daniel L. Polanco <dlpolanco05@gmail.com>
2015-04-25 15:49:22 -0600

    Refine logic pre-recursive function for populate_edge

--- pclouds.hpp
+++ pclouds.hpp
@@ -23,7 +23,7 @@ class cloud : public sequence::kmer {
 
 	public:
 	cloud( const sequence::kmer &root );
-	std::set <sequence::kmer> find_edge( const unsigned int shell, const std::set <sequence::kmer> &kmers ) const;
+	std::set <sequence::kmer> populate_edge( const unsigned int shell, const std::set <sequence::kmer> &kmers ) const;
 
 	void json( nlohmann::json );
 	nlohmann::json json( );
@@ -36,33 +36,32 @@ std::set <cloud> build( const parameters param, const std::string &sequence ) {
 
 	std::set <cloud> clouds = get_core( param.core, kmers );
 	for (auto &cloud: clouds) {
-		kmers = cloud.find_edge( param.shell, kmers );
+		kmers = cloud.populate_edge( param.shell, kmers );
 	}
 
 	return clouds;
 }
 
-std::set <sequence::kmer> cloud::find_edge( const unsigned int shell, const std::set <kmer> &kmers ) const {
+std::set <sequence::kmer> cloud::populate_edge( const unsigned int shell, const std::set <kmer> &kmers ) const {
 	unsigned int threads = std::thread::hardware_concurrency( );
 	if ( threads > 1 )
 		threads = threads - 1;
 	ThreadPool pool( threads );
 
-	std::set <sequence::kmer> leftover = kmers;
 	// recurse function
 	// algorithm: pursue any that are at least 3 nucleotides away
-	// branch: if < 3 away, start new recurse at position
+	// branch: if < 3 away, add to this->kmers and start new recurse at position
 	// finish: once count is below threshold, stop
 
-
+	std::set <sequence::kmer> leftover = kmers;
 	leftover.erase( kmers.find( this->root) );
 	std::remove_if( leftover.begin(), leftover.end(),
 			[&](const std::set<sequence::kmer>::iterator kmer_iter){
-				return leftover.count( *kmer_iter );
+				return this->kmers.count( *kmer_iter );
 			});
 	return leftover;
 }
 
 
 }
-}
+i
