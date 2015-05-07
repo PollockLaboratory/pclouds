dee129d60a8db43effcd0c51f7b47cd0f307b363
Daniel L. Polanco <dlpolanco05@gmail.com>
2015-05-07 12:35:35 -0600

    Add some notes and fix a few small bugs from STP's help

--- pclouds.hpp
+++ pclouds.hpp
@@ -13,18 +13,14 @@ namespace grizzly {
 namespace pclouds {
 
 struct parameters {
-	unsigned int core;
-	unsigned int shell;
+	unsigned int core{ };
+	unsigned int shell{ };
+	unsigned int kmer_length{ };
 };
 
 class cloud {
-	const sequence::kmer root;
-	std::vector <sequence::kmer> kmers;
-
-	std::vector <sequence::kmer>
-		recurse_edges( const std::vector <sequence::kmer>::iterator &begin,
-				const std::vector <sequence::kmer>::iterator &end,
-				const unsigned int threshold );
+	sequence::kmer root;
+	std::vector <sequence::kmer> kmers{ };
 
 	public:
 	cloud( const sequence::kmer &root );
@@ -36,14 +32,16 @@ class cloud {
 };
 
 std::vector <cloud> build( const parameters param, const std::string &sequence );
+// get starting clouds
 std::vector <cloud> generate_clouds( const unsigned int &core_threshold, const std::vector <sequence::kmer> &kmers );
+// find any new clouds and edges
 std::vector <cloud> expand_clouds( const unsigned int &shell_threshold, const std::vector <cloud> &clouds, const std::vector <sequence::kmer> &kmers );
-void expand_cloud(const unsigned int &threshold, cloud &cloud, ThreadPool &pool,
+void recurse_edges(const unsigned int &threshold, cloud &cloud, ThreadPool &pool,
 		std::vector <sequence::kmer>::const_iterator start,
 		std::vector <sequence::kmer>::const_iterator end);
 
 std::vector <cloud> build( const parameters param, const std::string &sequence ) {
-	std::vector <sequence::kmer> kmers = sequence::count( sequence );
+	std::vector <sequence::kmer> kmers = sequence::count( sequence, param.kmer_length );
 
 	std::vector <cloud> clouds = generate_clouds( param.core, kmers );
 	clouds = expand_clouds( param.shell, clouds, kmers );
@@ -58,7 +56,7 @@ std::vector <cloud> generate_clouds( const unsigned int &core_threshold, const s
 	std::vector <cloud> clouds;
 	for (const auto &kmer: sorted) {
 		if (kmer.get_count() > core_threshold)
-				clouds.push_back( cloud( kmer ));
+			clouds.push_back( cloud( kmer ));
 	};
 	return clouds;
 }
@@ -69,14 +67,22 @@ std::vector <cloud> expand_clouds( const unsigned int &shell_threshold, const st
 		threads = threads - 1;
 	ThreadPool pool( threads );
 
+	// after setting up our thread pool, we then want to recursively search cloud space
+	// for each cloud. So we search and if we find another kmer that should be a cloud,
+	// we call it a cloud, initialize it, and add it to our thread pool of clouds to search
 	std::vector <cloud> new_clouds = clouds;
 	for (auto &cloud: new_clouds) {
-		expand_cloud( shell_threshold, cloud, pool, kmers.cbegin(), kmers.cend() );
+		// this is the function that is recursive
+		recurse_edges( shell_threshold, cloud, pool, kmers.cbegin(), kmers.cend() );
 	}
+	// right now, it's possible (and will happen) that a core kmer will become part of another cloud
+	// we may want to change this behavior later, but right now it doesn't matter since in the end
+	// all we care about is if the kmer is there or not.
+
 	return new_clouds;
 }
 
-void expand_cloud( const unsigned int &threshold, cloud &cloud, ThreadPool &pool,
+void recurse_edges( const unsigned int &threshold, cloud &cloud, ThreadPool &pool,
 		std::vector <sequence::kmer>::const_iterator start,
 		std::vector <sequence::kmer>::const_iterator end ) {
 	// algorithm: if <= 3 away and count > threshold, add to this->kmers and start new recurse at position
@@ -84,15 +90,24 @@ void expand_cloud( const unsigned int &threshold, cloud &cloud, ThreadPool &pool
 
 	std::mutex cloud_lock;
 	sequence::kmer root_kmer = cloud.get_root_kmer();
-	for ( auto kmer = start; kmer < end; ++kmer ) {
-		if ( sequence::distance (kmer->get_sequence(), root_kmer.get_sequence()) <= 3 && kmer->get_count() > threshold ) {
-			cloud_lock.lock(); // move to add_kmer ?
-			cloud.add_kmer( *kmer );
+	for ( auto kmer_iter = start; kmer_iter < end; ++kmer_iter ) {
+		if ( sequence::distance (kmer_iter->get_sequence(), root_kmer.get_sequence()) <= 3 && kmer_iter->get_count() > threshold ) {
+			// lock the cloud so we don't get a race condition in any of our branching recurse_edges-es
+			cloud_lock.lock();
+			cloud.add_kmer( *kmer_iter );
 			cloud_lock.unlock();
 
-			pool.enqueue( expand_cloud, threshold, cloud, pool, ++kmer, end );
+			auto status = pool.enqueue( recurse_edges, threshold, cloud, pool, ++kmer_iter, end );
+			status.get(); // wait for current recursive branch to finish
+			// I don't like my logic here. I would prefer to move this up into expand_clouds
+			// Plus, shouldn't the pool automatically finish and start new jobs on its own?
+			// It might, in which case, status.get() just means wait if it hasn't already finished
 		}
 	}
+}
+
+cloud::cloud( const sequence::kmer &root ) {
+	this->root = root;
 }
 
 }
